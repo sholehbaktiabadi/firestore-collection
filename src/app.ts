9352c9e84ab6739ddb553f139438453cd5dc20b2
bektidev <bekti@primaku.com>
2024-01-12 22:12:10 +0700

    feat: add api key authorization middleware

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,14 @@ import koaBody from "koa-bodyparser";
 import cors from '@koa/cors'
 import { firestoreRoutes } from "./service/firestore.route";
 import env from "./config/env";
+import { ApiKeyMiddleware } from "./middleware/api-key";
 
 function bootstarp() {
   const app = new Koa();
   app.use(cors())
+  app.use(ApiKeyMiddleware)
   app.use(koaBody());
+  app.keys = ['authKey']
   app.use(firestoreRoutes.routes());
   app.use((ctx) => {
     ctx.body = { status: "ok" };
--- src/config/env.ts
+++ src/config/env.ts
@@ -5,6 +5,7 @@ const env = () => ({
   APP_PORT: +process.env.APP_PORT,
   FIRESTORE_PROJECT_ID: process.env.FIRESTORE_PROJECT_ID,
   FIRESTORE_SECRET_PATH: process.env.FIRESTORE_SECRET_PATH,
+  APP_X_API_KEY: process.env.APP_X_API_KEY
 });
 
 export default env();
